607c84eaf4d10ee9ecf0b4ea05d58b13b85dfda6
LorenzoBondan <lbbondan@gmail.com>
2023-03-01 14:21:35 -0300

    education and experience

--- src/components/AboutMe/PersonalData/index.tsx
+++ src/components/AboutMe/PersonalData/index.tsx
@@ -3,6 +3,9 @@ import { MdEmail } from 'react-icons/md';
 import { AiFillPhone } from 'react-icons/ai';
 import { BsCalendarDateFill } from 'react-icons/bs';
 import ucsLogo from 'assets/images/ucs.jpg';
+import logoSagrado from 'assets/images/logo-sagrado.png';
+import logoTodeschini from 'assets/images/todeschini.png';
+import logoWeg from 'assets/images/weg.png';
 import './styles.css';
 
 function ageCalc(date: string | undefined){
@@ -24,33 +27,82 @@ const PersonalData = () => {
                 <h4><i><BsCalendarDateFill/></i> Age - <span>{ageCalc("1999-07-17")}</span></h4>
             </div>
 
-            <div className='education-container'>
-                <h2>Education</h2>
-                
-                <div className='college-container'>
+            <div className='education-and-experience-container'>
 
-                    <div className='college-card'>
-                        <div className='college-card-img'>
-                            <img src={ucsLogo} alt="UCS" />
+                <div className='education-container'>
+                    <h2>Education</h2>
+                    
+                    <div className='college-container'>
+
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src={ucsLogo} alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>Software Engineering</h4>
+                                <h6>Current: 1/8</h6>
+                            </div>
                         </div>
-                        <div className='college-card-content'>
-                            <h4>Software Engineering</h4>
-                            <h6>Current: 1/8</h6>
+
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src={ucsLogo} alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>Mechanical Engineering</h4>
+                                <h6>Current: 10/10</h6>
+                            </div>
                         </div>
+
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src={logoSagrado} className="marista" alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>High School</h4>
+                            </div>
+                        </div>
+
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src="https://apps.colegiomarista.org.br/matricula/aparecida/library/midias/imagens/logotipo.png" className='marista' alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>Elementary School</h4>
+                            </div>
+                        </div>
+
                     </div>
-
-                    <div className='college-card'>
-                        <div className='college-card-img'>
-                            <img src={ucsLogo} alt="UCS" />
-                        </div>
-                        <div className='college-card-content'>
-                            <h4>Mechanical Engineering</h4>
-                            <h6>Current: 10/10</h6>
-                        </div>
-                    </div>
-
                 </div>
+
+                <div className='experience-container'>
+                    <h2>Experience</h2>
+
+                    <div className='professional-container'>
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src={logoTodeschini} alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>Engineering Assistent</h4>
+                                <h6>1 year and 3 months</h6>
+                            </div>
+                        </div>
+
+                        <div className='college-card'>
+                            <div className='college-card-img'>
+                                <img src={logoWeg} alt="UCS" />
+                            </div>
+                            <div className='college-card-content'>
+                                <h4>Engineering Trainee</h4>
+                                <h6>6 months</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
         </div>
         </>
     );
