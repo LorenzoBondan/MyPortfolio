a819f4f0a62eb7c9fe049c5624e7892cbd9511ce
LorenzoBondan <lbbondan@gmail.com>
2023-07-01 14:30:18 -0300

    created modal to show project details

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import githubIcon from 'assets/images/github.svg';
 import './styles.css';
 
 type Props = {
@@ -5,30 +10,84 @@ type Props = {
     description: string;
     imgUrl: string;
     projectUrl: string;
+    images: {url: string}[];
 }
 
-export const ProjectCard = ({ title, description, imgUrl, projectUrl }: Props) => {
+export const ProjectCard = ({ title, description, imgUrl, projectUrl, images }: Props) => {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal(){
+    setModalIsOpen(true);
+  }
+
+  function closeModal(){
+    setModalIsOpen(false);
+  }
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+};
+
   return (
     <div className='project-card'>
-        
       <div className='card-top-container'>
           <img src={imgUrl} alt="" />
       </div>
-
       <div className='card-bottom-container-project-card'>
-
           <div className='card-title-bottom'>
             <h3>{title}</h3>
             <p>{description}</p>
-
-              <a href={projectUrl}>
-                  <div className='button-zone'>
-                      <button className='btn btn-primary'>Details</button>
+            <div className='button-zone'>
+              <button className='btn btn-primary' onClick={openModal}>Details</button>
+              <Modal 
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Example Modal"
+                overlayClassName="modal-overlay"
+                className="project-modal"
+              >
+                <div className='project-modal-container base-card'>
+                  <div className='project-modal-title'>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
                   </div>
-              </a>
-            
+                  <div className='project-modal-carousel-container'>
+                    <Carousel responsive={responsive} infinite={true} className="project-images-slider" autoPlay={true} autoPlaySpeed={3000}>
+                      {images.map((image, index) => (
+                        <div className='project-modal-image-container' key={index}>
+                          <img src={image.url} alt=""/>
+                        </div>
+                      ))}
+                    </Carousel>
+                  </div>
+                  <div className='project-modal-buttons-container'>
+                    <button className='btn btn-secondary' onClick={closeModal}>Close</button>
+                    <a href={projectUrl} className='project-link' target="_blank" rel="noopener noreferrer">
+                      <button className='btn btn-secondary'><img src={githubIcon} alt="" /> See Github Project</button>
+                    </a>
+                  </div>
+                  
+                </div>
+                
+              </Modal>
+            </div>
           </div>
-        
       </div>
 </div>
   )
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,7 +3,6 @@ import { ProjectCard } from 'components/ProjectCard';
 import { Nav, Row, Tab } from 'react-bootstrap';
 import './styles.css';
 
-
 const Projects = () => {
 
     const projects1 = [
@@ -11,37 +10,226 @@ const Projects = () => {
           title: "AgiTask",
           description: "Tasks management tool, with comments, notifications and data analysis",
           imgUrl: "https://user-images.githubusercontent.com/105743965/250280313-8e2fb385-dedb-4e50-88c3-3aa54d6cc0ae.png",
-          projectUrl: "https://github.com/LorenzoBondan/AgiTask"
+          projectUrl: "https://github.com/LorenzoBondan/AgiTask",
+          images:[
+            {
+              url: "https://user-images.githubusercontent.com/105743965/250280313-8e2fb385-dedb-4e50-88c3-3aa54d6cc0ae.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522590-64ca646b-1297-4870-a10d-4d1ba2a93d62.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522596-fa5bf119-780b-4cb0-90a5-b77493d0df06.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522597-324b2df0-6cbf-4e9e-ada1-cae2184a34fb.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522603-3fe949b7-58fd-4ad7-a01c-ee009b78e84a.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522610-72838052-c4b8-4c1b-b681-d8d77f5269f7.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522616-4ea1d391-de29-4ba2-8cbd-543901db044f.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522620-3725c1bd-b2bf-4e4a-9e80-2c539cfd59d3.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522622-8e72b43b-6053-4f15-9f46-2884f9faa2f3.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522625-0a3ece16-77ba-4cb3-aaa3-627558b54811.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522630-c8b8d573-4ae3-4ac7-9f7f-1d99cee9e940.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522638-a10339d6-0d4a-4129-a60a-e4dfe3840cb1.png",
+            },
+            {
+              url: "https://user-images.githubusercontent.com/105743965/248522643-e5e2fe29-a4c9-4136-9abf-0630b5846ea9.png",
+            },
+          ]
         },
         {
           title: "Social Network",
           description: "A Social Network where users can post, follow, like and comment their friends posts.",
           imgUrl: "https://user-images.githubusercontent.com/105743965/241565403-db0c85ec-9e48-4abe-a172-20b496399648.png",
-          projectUrl: "https://github.com/LorenzoBondan/SocialNetwork"
+          projectUrl: "https://github.com/LorenzoBondan/SocialNetwork",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "Soccer League Simulator",
           description: "A summary and a simulator of the brazilian soccer league",
           imgUrl: "https://user-images.githubusercontent.com/105743965/233791700-2fc0d1e8-5224-482c-ba43-3b6021b12a7e.png",
-          projectUrl: "https://github.com/LorenzoBondan/SoccerLeague"
+          projectUrl: "https://github.com/LorenzoBondan/SoccerLeague",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "High School Class",
           description: "A summary of the author's high school class",
           imgUrl: "https://user-images.githubusercontent.com/105743965/219984126-4d08a00e-a9f5-4b8a-a803-e8deefd8da9e.png",
-          projectUrl: "https://github.com/LorenzoBondan/HighSchool-Students"
+          projectUrl: "https://github.com/LorenzoBondan/HighSchool-Students",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "University",
           description: "A university project where students can subscribe to courses and class sections of the subjects.",
           imgUrl: "https://user-images.githubusercontent.com/105743965/239691813-a33afe1c-557f-44e2-8d0a-56ec155b1d3b.png",
-          projectUrl: "https://github.com/LorenzoBondan/University"
+          projectUrl: "https://github.com/LorenzoBondan/University",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "BarberShop",
           description: "A BarberShop where clients can create new appointments with their preferred barber at a convenient time for them.",
           imgUrl: "https://user-images.githubusercontent.com/105743965/244556720-0d134571-8bab-473a-a27b-016248f89d39.png",
-          projectUrl: "https://github.com/LorenzoBondan/BarberShop"
+          projectUrl: "https://github.com/LorenzoBondan/BarberShop",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
     ]
 
@@ -50,25 +238,57 @@ const Projects = () => {
           title: "DS Catalog",
           description: "A catalog of products for sale",
           imgUrl: "https://i.ibb.co/D97jYg9/dscatalog.png",
-          projectUrl: "https://github.com/LorenzoBondan/dscatalog"
+          projectUrl: "https://github.com/LorenzoBondan/dscatalog",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "MovieFlix",
           description: "A movie site with reviews for registered members",
           imgUrl: "https://i.ibb.co/0KHHbfd/movieflix.png",
-          projectUrl: "https://github.com/LorenzoBondan/MovieFlix"
+          projectUrl: "https://github.com/LorenzoBondan/MovieFlix",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "DS Learn",
           description: "Backend project of a programming school website",
           imgUrl: "https://i.ibb.co/8g1Mw0c/dslearn.png",
-          projectUrl: "https://github.com/LorenzoBondan/dslearn"
+          projectUrl: "https://github.com/LorenzoBondan/dslearn",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "Delivery App",
           description: "Application of an delivery service",
           imgUrl: "https://i.ibb.co/6wLM9Vm/deliver.png",
-          projectUrl: "https://github.com/LorenzoBondan/Delivery"
+          projectUrl: "https://github.com/LorenzoBondan/Delivery",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
 
     ]
@@ -78,31 +298,71 @@ const Projects = () => {
           title: "DS Sales",
           description: "Dashboard of sales",
           imgUrl: "https://i.ibb.co/txGmKd0/dssales.png",
-          projectUrl: "https://github.com/LorenzoBondan/DSSales"
+          projectUrl: "https://github.com/LorenzoBondan/DSSales",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
         title: "Search for Github User",
         description: "Application to fetch user data from github using Github API",
         imgUrl: "https://i.ibb.co/HxT2qFd/githubuser.png",
-        projectUrl: "https://github.com/LorenzoBondan/Search-for-GitHub-User"
+        projectUrl: "https://github.com/LorenzoBondan/Search-for-GitHub-User",
+        images:[
+          {
+            url: "",
+          },
+          {
+            url: "",
+          },
+        ]
         },
         {
           title: "Final Paper of Mechanical Engineering",
           description: "Frequency and sound pressure level meter that generates dosimetry reports",
           imgUrl: "https://i.ibb.co/qYvXjTc/dosimetria.png",
-          projectUrl: "https://github.com/LorenzoBondan"
+          projectUrl: "https://github.com/LorenzoBondan",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "Forniture structure automation",
           description: "C# Windows Forms CSV handling and a file return ready for import into the company's system",
           imgUrl: "https://i.ibb.co/TgLSn4b/pintura.png",
-          projectUrl: "https://github.com/LorenzoBondan/Estrutura-Pintura"
+          projectUrl: "https://github.com/LorenzoBondan/Estrutura-Pintura",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         },
         {
           title: "Packaging structure automation",
           description: "C# Windows Forms CSV handling and a file return ready for import into the company's system",
           imgUrl: "https://i.ibb.co/X7w5RYs/fantasma.png",
-          projectUrl: "https://github.com/LorenzoBondan/Estrutura-Embalagem"
+          projectUrl: "https://github.com/LorenzoBondan/Estrutura-Embalagem",
+          images:[
+            {
+              url: "",
+            },
+            {
+              url: "",
+            },
+          ]
         }
     ]
 
